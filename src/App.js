f7daeb7cab7488d1f9f5b589fdc95604a959c005
Anderson Ferreira Alves <andersonamericasul07@gmail.com>
2022-04-21 20:47:16 -0300

    feat: roteamento

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import SignInPage from './pages/SignIn'
+import SignUpPage from './pages/SignUp'
+import HomePage from './pages/Home'
+import EditProfilePage from './pages/EditProfile'
 
-function App() {
+function App () {
   return (
     <div >
-      Projeto
+      <Routes>
+        <Route path="/" element={<SignInPage />} />
+        <Route path="sign_up" element={<SignUpPage />} />
+        <Route path="home" element={<HomePage />} />
+        <Route path="edit_profile" element={<EditProfilePage />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
