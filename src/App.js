9dae7b41f09852b6b9fd2c8933aac3136c01e5a7
Anderson Ferreira Alves <andersonamericasul07@gmail.com>
2022-04-22 00:19:34 -0300

    feat: pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import SignInPage from './pages/SignIn'
-import SignUpPage from './pages/SignUp'
-import HomePage from './pages/Home'
-import EditProfilePage from './pages/EditProfile'
+import SignInPage from './routes/signin'
+import SignUpPage from './routes/signup'
+import HomePage from './routes/home'
+import ProfilePage from './routes/profile'
 
 function App () {
   return (
@@ -12,7 +12,7 @@ function App () {
         <Route path="/" element={<SignInPage />} />
         <Route path="sign_up" element={<SignUpPage />} />
         <Route path="home" element={<HomePage />} />
-        <Route path="edit_profile" element={<EditProfilePage />} />
+        <Route path="edit_profile" element={<ProfilePage />} />
       </Routes>
     </div>
   )
