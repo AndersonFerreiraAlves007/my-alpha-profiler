dbe40caff943fff92a854f7e1ce395f8b4610e14
Jaime <jaume.lan@gmail.com>
2022-04-28 08:10:01 -0300

    home page with context with some funcionalities

---
+++ src/components/HomeBody/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Div = styled.div`
+width: 100%;
+height: 100vh;
+background: white;
+color: black;
+display: flex;
+flex-direction: column;
+align-items: center;
+padding: 30px 40px;
+`
