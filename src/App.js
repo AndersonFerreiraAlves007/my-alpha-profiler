1b73f7cb6a7d4cac2a282ac3bfe680a7fffd7afb
Jaime <jaume.lan@gmail.com>
2022-05-01 23:45:08 -0300

    clean up of app

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
-import React/* , { useContext } */ from 'react'
-import { Routes, Route/* , useNavigate */ } from 'react-router-dom'
-// import { AuthContext } from './context/auth.context.jsx'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import SignInPage from './pages/signin'
 import SignUpPage from './pages/signup'
 import ProfilePage from './pages/profile'
 import HomePage from './pages/home/index.jsx'
 
 function App () {
-  /* const { authenticated } = useContext(AuthContext)
-  const navigate = useNavigate() */
   return (
     <div>
       <Routes>
         <Route exact path="/" element={<SignInPage />} />
         <Route exact path="sign_up" element={<SignUpPage />} />
-        {/* {authenticated
-          ? ( */}
-          <Route exact path="home" element={<HomePage />} />
-          {/*   )
-          : (
-              navigate('/')
-            )} */}
-        {/* {authenticated
-          ? ( */}
-          <Route exact path="edit_profile" element={<ProfilePage />} />
-          {/*   )
-          : (
-              navigate('/')
-            )} */}
+        <Route exact path="home" element={<HomePage />} />
+        <Route exact path="edit_profile" element={<ProfilePage />} />
       </Routes>
     </div>
   )
